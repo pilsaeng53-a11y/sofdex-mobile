2105ce027a9ff7d8c110f0928ceb7608ffabe085
필생 <pilsaeng53@gmail.com>
2026-03-14 14:15:33 +0000

    File changes

--- src/pages/SolFort.jsx
+++ src/pages/SolFort.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import {
   TrendingUp, Zap, Shield, Globe, BarChart3, Users,
